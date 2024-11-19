a37683211bf83f1bcb116ce4161580e83060f6cb
lizaloraine <lizaloraine124@gmail.com>
2024-11-19 20:26:14 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 3 - 19-11-2024/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const textInput = document.getElementById('textInput');
+    const wordCount = document.getElementById('wordcount');
+    const sentenceCount = document.getElementById('sentenceCount');
+  
+    textInput.addEventListener('input', () => {
+      const text = textInput.value.trim();
+  
+      const words = text.split(/\s+/).filter(word => word.length > 0);
+      wordCount.textContent = `Words: ${words.length}`;
+  
+      const sentences = text.split(/[.!?]\s+/).filter(sentence => sentence.length > 0);
+      sentenceCount.textContent = `Sentences: ${sentences.length}`;
+    });
+  });
+  
